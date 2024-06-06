9a1d2743749d3e2446979e8196aa7ec9f0defe79
Adel Bensalem <adel.bensalem@epitech.eu>
2024-06-07 01:52:37 +0200

    replace paddle rect by sprite

--- scenes/game/paddle/paddle.gd
+++ scenes/game/paddle/paddle.gd
@@ -1,4 +1,8 @@
 extends CharacterBody2D
 
+@export var flipped: bool = false:
+	set(is_flipped):
+		$Sprite2D.flip_h = is_flipped
+
 func get_size():
-	return $ColorRect.size
+	return $Sprite2D.get_rect().size
